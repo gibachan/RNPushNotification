9e3471b61c8625c3247a67130562be33988730d8
gibachan <childhoodend.tk@gmail.com>
2020-08-10 22:36:12 +0900

    Add handling interaction

--- App.tsx
+++ App.tsx
@@ -8,21 +8,41 @@
  * @format
  */
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import messaging from '@react-native-firebase/messaging'; // これを追加する
 
 const App = () => {
+  const [message, setMessage] = useState('まだ通知を受信していません');
+
   useEffect(() => {
     // `wether`トピックを購読
     messaging()
       .subscribeToTopic('weather')
       .then(() => console.log('Subscribed to topic!'));
+
+    // アプリがバックグラウンドで実行中に通知をタップされたときのコールバック
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      setMessage(
+        `[${remoteMessage.notification?.title}]によってバックグラウンドから復帰しました`,
+      );
+    });
+
+    // 通知をタップしてアプリが起動されたときのコールバック
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          setMessage(
+            `[${remoteMessage.notification?.title}]によって起動されました`,
+          );
+        }
+      });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>{message}</Text>
     </View>
   );
 };

674cfe32f44a607c11830a55fb73a1d01bf8e801
gibachan <childhoodend.tk@gmail.com>
2020-08-10 22:28:57 +0900

    Add callback for background state

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging'; // これを追加する
+
+// バックグラウンドで通知を受信した時のコールバック
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  `[${remoteMessage.notification?.title}]を受信しました`;
+});
 
 AppRegistry.registerComponent(appName, () => App);
